b5cfbbad309277a95a4146e0292fc6993e1e4823
Elanora Manson <git@elanora.lol>
2025-02-06 14:26:50 -0800

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "A very basic flake to control my universe";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    { self, nixpkgs, home-manager, ... }@attrs:
+    {
+      nixosConfigurations = {
+        inanna = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          specialArgs = attrs;
+          modules = [
+            ./inanna-configuration.nix
+            home-manager.nixosModules.home-manager {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+
+              home-manager.users.el = import ./el-home.nix;
+            }
+          ];
+        };
+      };
+    };
+}
---
+++ inanna-configuration.nix
@@ -0,0 +1,196 @@
+# configuration.nix(5)
+# https://search.nixos.org/options
+# nixos-help
+
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
+{
+  imports = [
+    ./hardware-configuration.nix # Include the results of the hardware scan.
+  ];
+
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
+
+  nixpkgs.config = {
+    allowUnfree = true;
+    # For Sonarr v4
+    permittedInsecurePackages = [
+      "dotnet-sdk-6.0.428"
+      "dotnet-sdk-wrapped-6.0.36"
+      "aspnetcore-runtime-6.0.36"
+      "aspnetcore-runtime-wrapped-6.0.428"
+    ];
+  };
+
+  boot = {
+    loader = {
+      systemd-boot.enable = true;
+      efi.canTouchEfiVariables = true;
+    };
+
+    kernelParams = [ "nvidia-drm.modeset=1" ];
+  };
+
+  networking = {
+    hostName = "inanna";
+    networkmanager.enable = true;
+
+    firewall.allowedTCPPorts = [ 8000 3030 ];
+  };
+
+  time.timeZone = "America/Los_Angeles";
+
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  services = {
+    plex = {
+      enable = true;
+      openFirewall = true;
+      user = "el";
+    };
+
+    jellyfin = {
+	enable = true;
+	user = "el";
+    };
+
+    radarr = {
+      enable = true;
+      openFirewall = true;
+      user = "el";
+    };
+
+    sonarr = {
+      enable = true;
+      openFirewall = true;
+      user = "el";
+      package = pkgs.sonarr.overrideAttrs (lib.const { doCheck = false; });
+    };
+
+    prowlarr = {
+	enable = true;
+	openFirewall = true;
+    };
+
+    tailscale.enable = true;
+
+    xserver.videoDrivers = [ "nvidia" ];
+
+    displayManager.sddm = {
+      enable = true;
+      wayland.enable = true;
+    };
+
+    desktopManager.plasma6.enable = true;
+
+    pipewire = {
+      enable = true;
+      pulse.enable = true;
+      jack.enable = true;
+      wireplumber.enable = true;
+    };
+
+    openssh.enable = true;
+
+    printing.enable = true;
+
+    hardware.openrgb = {
+      enable = true;
+      package = pkgs.openrgb-with-all-plugins;
+      motherboard = "amd";
+    };
+  };
+
+  hardware = {
+    graphics.enable = true;
+
+    nvidia = {
+      modesetting.enable = true;
+      powerManagement.finegrained = false;
+      open = false;
+      nvidiaSettings = true;
+      package = config.boot.kernelPackages.nvidiaPackages.beta;
+    };
+  };
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users = {
+    defaultUserShell = pkgs.zsh;
+
+    users.el = {
+      isNormalUser = true;
+      extraGroups = [ "wheel" ];
+    };
+  };
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    bottom
+    fd
+    lsd
+    wget
+    ripgrep
+    watchman
+  ];
+
+  environment.pathsToLink = [ "/share/zsh" ];
+
+  programs = {
+    git = {
+      enable = true;
+      config = {
+        init.defaultBranch = "main";
+      };
+    };
+
+    neovim = {
+      enable = true;
+      defaultEditor = true;
+      viAlias = true;
+      vimAlias = true;
+      withNodeJs = true;
+      withPython3 = true;
+      withRuby = true;
+    };
+
+    zsh = {
+      enable = true;
+      autosuggestions.enable = true;
+      enableCompletion = true;
+      syntaxHighlighting.enable = true;
+    };
+  };
+
+  # Copy the NixOS configuration file and link it from the resulting system
+  # (/run/current-system/configuration.nix). This is useful in case you
+  # accidentally delete configuration.nix.
+  # system.copySystemConfiguration = true;
+
+  # This option defines the first version of NixOS you have installed on this particular machine,
+  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
+  #
+  # Most users should NEVER change this value after the initial install, for any reason,
+  # even if you've upgraded your system to a new NixOS release.
+  #
+  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
+  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
+  # to actually do that.
+  #
+  # This value being lower than the current NixOS release does NOT mean your system is
+  # out of date, out of support, or vulnerable.
+  #
+  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
+  # and migrated your data accordingly.
+  #
+  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
+  system.stateVersion = "24.05"; # Did you read the comment?
+}
