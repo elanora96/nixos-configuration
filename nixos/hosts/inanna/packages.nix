2aa05daeadc02b47657d666e37aaad4c44c0a18f
Elanora Manson <git@elanora.lol>
2026-02-24 13:39:10 -0800

    Keep-sorted

--- nixos/hosts/inanna/packages.nix
+++ nixos/hosts/inanna/packages.nix
@@ -3,10 +3,12 @@
   imports = [ ../../modules/minimal-packages.nix ];
 
   environment.systemPackages = with pkgs; [
+    # keep-sorted start
     ntfs2btrfs
     uv
     watchman
     waypipe
     wireguard-tools
+    # keep-sorted end
   ];
 }
