7518257f3e187be62f5245173280d25fa4943123
Elanora Manson <git@elanora.lol>
2026-02-24 13:38:23 -0800

    Keep-sorted, add dust, swap nixfmt-rfc-style for new nixfmt

--- nixos/modules/minimal-packages.nix
+++ nixos/modules/minimal-packages.nix
@@ -1,13 +1,17 @@
+# Packages I want on every system
 { pkgs, ... }:
 
 {
   environment.systemPackages = with pkgs; [
+    # keep-sorted start
     bottom
+    dust
     fd
     lsd
-    nixfmt-rfc-style
+    nixfmt
     ripgrep
     wget
     zellij
+    # keep-sorted end
   ];
 }
