5baa1e6f27eaa7462b15fac1a2d7d8c5e1b3df5f
Elanora Manson <git@elanora.lol>
2026-02-24 13:35:20 -0800

    Add comments

--- nixos/hardware/cpu-amd-r7-3700x/default.nix
+++ nixos/hardware/cpu-amd-r7-3700x/default.nix
@@ -3,7 +3,10 @@
 {
   imports = [
     inputs.nixos-hardware.nixosModules.common-cpu-amd
+    # Enables the amd cpu scaling https://www.kernel.org/doc/html/latest/admin-guide/pm/amd-pstate.html
     inputs.nixos-hardware.nixosModules.common-cpu-amd-pstate
+    # Enables the zenpower sensor in lieu of the k10temp sensor on Zen CPUs https://git.exozy.me/a/zenpower3
+    # On Zen CPUs zenpower produces much more data entries
     inputs.nixos-hardware.nixosModules.common-cpu-amd-zenpower
   ];
 }
