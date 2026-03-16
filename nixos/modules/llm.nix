efe4c7d8b83b34a56f7c0e95281ce1f14e74ca1e
Elanora Manson <git@elanora.lol>
2026-03-16 15:14:02 -0700

    Setup open-webui

--- nixos/modules/llm.nix
+++ nixos/modules/llm.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
 {
   services = {
     ollama = {
@@ -11,6 +11,17 @@
     open-webui = {
       enable = true;
       openFirewall = true;
+      port = 8182; # The audacity for the default port to be 8080
+      environment =
+        let
+          inherit (config.services.ollama) host port;
+        in
+        {
+          OLLAMA_API_BASE_URL = "http://${host}:${toString port}/api";
+          WEBUI_AUTH = "False";
+          RAG_EMBEDDING_ENGINE = "ollama";
+          RAG_EMBEDDING_MODEL = "qwen3-embedding:0.6b";
+        };
     };
   };
 
