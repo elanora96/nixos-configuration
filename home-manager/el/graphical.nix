11a6cda5fec11ccd2c8a7fb3895a78a2ec1a8563
Elanora Manson <git@elanora.lol>
2026-03-20 12:49:27 -0700

    zed dotfile

--- home-manager/el/graphical.nix
+++ home-manager/el/graphical.nix
@@ -26,7 +26,6 @@
 
   programs = {
     # keep-sorted start block=yes
-
     alacritty = {
       enable = true;
       theme = "gruvbox_material";
@@ -40,6 +39,65 @@
         sub-auto = "fuzzy";
       };
     };
+    # keep-sorted end
+  };
+
+  xdg.configFile."zed/settings.json".text = builtins.toJSON {
+    # keep-sorted start block=yes
+    "buffer_font_size" = 15;
+    "edit_predictions" = {
+      "mode" = "subtle";
+    };
+    "icon_theme" = "Colored Zed Icons Theme Dark";
+    "lsp" = {
+      "lua-language-server" = {
+        "settings" = {
+          "Lua" = {
+            "diagnostics" = {
+              "globals" = [ "vim" ];
+            };
+          };
+        };
+      };
+      "nixd" = {
+        "initialization_options" = {
+          "formatting" = {
+            "command" = [ "nixfmt" ];
+          };
+          "options" = {
+            # If this is ommited; default search path (<nixpkgs>) will be used.
+            "nixos" = {
+              "expr" =
+                "(builtins.getFlake \"/home/el/Projects/nixos-configuration\").nixosConfigurations.inanna.options";
+            };
+            # By default there is no home-manager options completion
+            "home_manager" = {
+              "expr" =
+                "(builtins.getFlake \"/home/el/Projects/nixos-configuration\").nixosConfigurations.inanna.options.home-manager.users.type.getSubOptions []";
+            };
+            # For flake-parts options.
+            "flake-parts" = {
+              "expr" = "(builtins.getFlake \"/home/el/Projects/nixos-configuration\").debug.options";
+            };
+            # For a `perSystem` flake-parts option =
+            "flake-parts2" = {
+              "expr" = "(builtins.getFlake \"/home/el/Projects/nixos-configuration\").currentSystem.options";
+            };
+          };
+        };
+      };
+    };
+    "soft_wrap" = "bounded";
+    "telemetry" = {
+      "metrics" = false;
+      "diagnostics" = false;
+    };
+    "theme" = {
+      "mode" = "system";
+      "light" = "One Light";
+      "dark" = "Gruvbox Material Neovim Dark";
+    };
+    "ui_font_size" = 16;
     # keep-sorted end
   };
 }
