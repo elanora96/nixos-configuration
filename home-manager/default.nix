2c950bbbe29b8d157f5f1e739d4a3a4edf7cf3b8
Elanora Manson <git@elanora.lol>
2026-03-22 20:03:39 -0700

    Move homeModules.common to home-manager/default.nix

--- home-manager/default.nix
+++ home-manager/default.nix
@@ -1,6 +1,31 @@
-_:
+{
+  config,
+  inputs,
+  lib,
+  pkgs,
+  ...
+}:
 
 {
-  programs.home-manager.enable = true; # Let Home Manager install and manage itself.
-  home.stateVersion = "24.05"; # Leave it alone!
+  imports = [ inputs.nixvim.homeModules.nixvim ];
+
+  home = {
+    # Sensible default for `home.homeDirectory`
+    homeDirectory = lib.mkDefault "/${
+      if pkgs.stdenv.isDarwin then "Users" else "home"
+    }/${config.home.username}";
+
+    # For macOS, $PATH must contain these.
+    sessionPath = lib.mkIf pkgs.stdenv.isDarwin [
+      "/etc/profiles/per-user/$USER/bin" # To access home-manager binaries
+      "/nix/var/nix/profiles/system/sw/bin" # To access nix-darwin binaries
+      "/usr/local/bin" # Some macOS GUI programs install here
+    ];
+
+    # Leave it alone!
+    stateVersion = "24.05";
+  };
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
 }
--- nixos/modules/home-manager.nix
+++ nixos/modules/home-manager.nix
@@ -1,27 +1,4 @@
-{
-  inputs,
-  lib,
-  ...
-}:
-let
-  home-modules = {
-    common =
-      { config, pkgs, ... }:
-      {
-        # Sensible default for `home.homeDirectory`
-        home.homeDirectory = lib.mkDefault "/${
-          if pkgs.stdenv.isDarwin then "Users" else "home"
-        }/${config.home.username}";
-
-        # For macOS, $PATH must contain these.
-        home.sessionPath = lib.mkIf pkgs.stdenv.isDarwin [
-          "/etc/profiles/per-user/$USER/bin" # To access home-manager binaries
-          "/nix/var/nix/profiles/system/sw/bin" # To access nix-darwin binaries
-          "/usr/local/bin" # Some macOS GUI programs install here
-        ];
-      };
-  };
-in
+{ inputs, ... }:
 {
   imports = [
     inputs.home-manager.nixosModules.home-manager
@@ -29,10 +6,7 @@ in
       home-manager.useGlobalPkgs = true;
       home-manager.useUserPackages = true;
       home-manager.backupFileExtension = "hm-bak";
-      home-manager.sharedModules = [
-        home-modules.common
-        inputs.nixvim.homeModules.nixvim
-      ];
+      home-manager.extraSpecialArgs = { inherit inputs; };
     }
   ];
 }
