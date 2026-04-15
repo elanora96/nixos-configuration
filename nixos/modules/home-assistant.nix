c40e824ed3d23596cd577349c7db88a7948dafa0
Elanora Manson <git@elanora.lol>
2026-04-15 09:08:27 -0700

    Add HA plugins and adjust config

--- nixos/modules/home-assistant.nix
+++ nixos/modules/home-assistant.nix
@@ -1,8 +1,18 @@
 { config, ... }:
+let
+  homekit = {
+    TCPPort = 21063;
+    UDPPort = 5353;
+  };
+in
 {
-  networking.firewall.allowedTCPPorts = [
-    config.services.home-assistant.config.http.server_port
-  ];
+  networking.firewall = {
+    allowedTCPPorts = [
+      config.services.home-assistant.config.http.server_port
+      homekit.TCPPort
+    ];
+    allowedUDPPorts = [ homekit.UDPPort ];
+  };
 
   services.home-assistant = {
     enable = true;
@@ -13,13 +23,24 @@
       "radio_browser"
 
       # User chosen components
-      "snapcast"
+      # keep-sorted start
+      "homekit"
+      "jellyfin"
       "opower"
+      "snapcast"
+      # keep-sorted end
     ];
     config = {
       # Includes dependencies for a basic setup
       # https://www.home-assistant.io/integrations/default_config/
       default_config = { };
+      logger = {
+        default = "WARNING";
+        logs = {
+          homeassistant.components.homekit = "DEBUG";
+          pyhap = "DEBUG";
+        };
+      };
     };
   };
 }
