e3e72b2f72850d8751cd90433722d0f2b128b551
Elanora Manson <git@elanora.lol>
2026-07-31 12:28:01 -0700

    Paperless improvements

--- nixos/modules/paperless.nix
+++ nixos/modules/paperless.nix
@@ -2,32 +2,30 @@
 let
   inherit (config) homelab;
   cfg = config.services.paperless;
+  domain = "paperless.${homelab.domain}";
 in
 {
   config = lib.mkIf homelab.enable {
-    networking.firewall = {
-      allowedTCPPorts = [ cfg.port ];
+    sops.secrets = {
+      "paperless/admin_password" = {
+        owner = cfg.user;
+        mode = "0440";
+      };
     };
 
     services.paperless = {
+      inherit domain;
       enable = true;
       consumptionDirIsPublic = true;
-
-      address = "0.0.0.0";
-      # domain = "https://paperless.${homelab.domain}";
-
+      passwordFile = config.sops.secrets."paperless/admin_password".path;
       settings = {
-        # Needed for CSRF etc
-        # PAPERLESS_URL = cfg.domain;
-        # PAPERLESS_CSRF_TRUSTED_ORIGIN = cfg.domain;
-        # PAPERLESS_CORS_ALLOWED_HOSTS = "http://localhost:${lib.toString cfg.port},${cfg.domain}";
-        # Ignore some junk
         PAPERLESS_CONSUMER_IGNORE_PATTERN = [
           ".DS_STORE/*"
           "desktop.ini"
+          "Thumbs.db"
         ];
         PAPERLESS_OCR_LANGUAGE = "eng";
-        PAPERLESS_FILENAME_FORMAT = "{{ owner_username }}/{{ correspondent }}/{{ created }} {{ title }}";
+        PAPERLESS_FILENAME_FORMAT = "{created_year}/{correspondent}/{title}";
         PAPERLESS_FILENAME_FORMAT_REMOVE_NONE = true;
         PAPERLESS_OCR_USER_ARGS = {
           optimize = 1;
@@ -35,6 +33,24 @@ in
         };
         # Used so that I can put files for a user into /consume/userA
         PAPERLESS_CONSUMER_RECURSIVE = true;
+      };
+    };
+
+    services.nginx = {
+      virtualHosts.${domain} = {
+        forceSSL = true;
+        useACMEHost = homelab.domain;
+        locations."/" = {
+          proxyPass = "http://127.0.0.1:${toString cfg.port}";
+          proxyWebsockets = true;
+          recommendedProxySettings = true;
+          extraConfig = ''
+            client_max_body_size 200M;
+            proxy_read_timeout 300s;
+            proxy_connect_timeout 300s;
+            proxy_send_timeout 300s;
+          '';
+        };
       };
     };
   };
