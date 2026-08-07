5836ee6fa11d21ff97f34610e966cab835ce8fd5
Elanora Manson <git@elanora.lol>
2026-08-07 13:22:49 -0700

    Let nixvim track seperate nixpkgs to suppress warning
    Annoyed by this

--- flake.nix
+++ flake.nix
@@ -23,7 +23,9 @@
     # nixvim - Configure Neovim with Nix!
     nixvim = {
       url = "github:nix-community/nixvim";
-      inputs.nixpkgs.follows = "nixpkgs";
+      # Annoying warning for following
+      # I guess it will be disabled soon?
+      # inputs.nixpkgs.follows = "nixpkgs";
       inputs.flake-parts.follows = "flake-parts";
     };
 
