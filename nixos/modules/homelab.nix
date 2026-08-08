9ecb5ae6b23099d195bc526cf591ccfa71ac3dac
Elanora Manson <git@elanora.lol>
2026-08-08 10:16:21 -0700

    Internal domain index
    Generate html that contains links to nginx virtual hosts and a some
    other things I haven't proxied yet

--- nixos/modules/homelab.nix
+++ nixos/modules/homelab.nix
@@ -7,6 +7,20 @@
 
 let
   cfg = config.homelab;
+  # TODO: This shouldn't be a thing really
+  unproxiedServiceNamePort = {
+    # keep-sorted start block=yes
+    home-assistant = config.services.home-assistant.config.http.server_port;
+    lidarr = config.services.lidarr.settings.server.port;
+    ollama = config.services.ollama.port;
+    open-webui = config.services.open-webui.port;
+    prowlarr = config.services.prowlarr.settings.server.port;
+    qbittorrent-web = 7470;
+    radarr = config.services.radarr.settings.server.port;
+    sonarr = config.services.sonarr.settings.server.port;
+    syncthing = lib.removePrefix "127.0.0.1:" config.services.syncthing.guiAddress;
+    # keep-sorted end
+  };
 in
 {
   options.homelab = {
@@ -39,15 +53,11 @@ in
       qbittorrent-nox
     ];
 
-    networking.firewall.allowedTCPPorts =
-      let
-        qbittorrentPort = 7470;
-      in
-      [
-        80
-        443
-        qbittorrentPort
-      ];
+    networking.firewall.allowedTCPPorts = [
+      80
+      443
+      unproxiedServiceNamePort.qbittorrent-web
+    ];
 
     security.acme = {
       acceptTerms = true;
@@ -74,6 +84,10 @@ in
       };
       nginx = {
         enable = true;
+        recommendedTlsSettings = true;
+        recommendedOptimisation = true;
+        recommendedGzipSettings = true;
+        recommendedProxySettings = true;
         virtualHosts.${cfg.domain} = {
           forceSSL = true;
           useACMEHost = cfg.domain;
@@ -81,20 +95,130 @@ in
           locations."/" =
             let
               index = "index.html";
+              virtualHosts = builtins.filter (virtualHost: virtualHost != "_") (
+                builtins.attrNames config.services.nginx.virtualHosts
+              );
+              proxiedLinks = map (service: "<a href=\"https://${service}\">${service}</a>") virtualHosts;
+              unproxiedLinks = lib.mapAttrsToList (
+                name: port:
+                let
+                  url = "http://inanna.internal:${toString port}";
+                in
+                "<a href=\"${url}\">${name} (${url})</a>"
+              ) unproxiedServiceNamePort;
+              toListItems = x: "<li>${builtins.concatStringsSep "</li><li>" x}</li>";
             in
             {
               inherit index;
               root = pkgs.writeTextDir index ''
-                <!DOCTYPE html lang="en">
-                <html>
+                <!DOCTYPE html>
+                <html lang="en">
                 <head>
-                  <meta charSet="UTF-8"/>
+                  <meta charset="UTF-8"/>
                   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+                  <style>
+                    :root {
+                      --highlight-border-radius:7px;
+                      --border-radius:11px;
+                      --yellow-highlight:#fffab7;
+                      --links:#0f6dff;
+                      --background-body:#fff;
+                      --background-main:#f1f1f1;
+                      --background-inputs:#fcfcfc;
+                      --text:#1c1d1e;
+                      --border:#ddd;
+                      --focus-highlight:#b8b8b8;
+                      --shadow-color:#545454;
+                    }
+                    @media (prefers-color-scheme:dark) {
+                      :root {
+                        --links:#4589ee;
+                        --background-body:#0f0f0f;
+                        --background-main:#222;
+                        --background-inputs:#222;
+                        --text:#efefef;
+                        --border:#444;
+                        --focus-highlight:#888;
+                        --shadow-color:#bebebe;
+                      }
+                    }
+                    *,
+                    :after,
+                    :before {
+                      box-sizing:border-box
+                    }
+                    html,
+                    body {
+                      color:var(--text);
+                      background:var(--background-body);
+                      margin:0;
+                      font-family:-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Oxygen-Sans,Ubuntu,Cantarell,Helvetica Neue,sans-serif;
+                      font-size:12pt
+                    }
+                    h1 {
+                      font-size:2.5rem
+                    }
+                    h2 {
+                      font-size:2rem
+                    }
+                    h3 {
+                      font-size:1.75rem
+                    }
+                    h4 {
+                      font-size:1.5rem
+                    }
+                    h5 {
+                      font-size:1.25rem;
+                      font-weight:400
+                    }
+                    h6 {
+                      font-size:1rem;
+                      font-weight:400
+                    }
+                    h1,
+                    h2,
+                    h3,
+                    h4,
+                    h5,
+                    h6 {
+                      margin-top:.5rem;
+                      margin-bottom:.5rem
+                    }
+                    h1,
+                    h2,
+                    h3,
+                    h4,
+                    b,
+                    strong,
+                    th {
+                      font-weight:700
+                    }
+                    li,
+                    p {
+                      line-height:1.6em
+                    }
+                    a {
+                      color:var(--links)
+                    }
+                    a:active,
+                    a:hover,
+                    a:focus {
+                      text-decoration:none
+                    }
+                  </style>
                   <title>${cfg.domain}</title>
                 </head>
                 <body>
-                  <h1>You have reached</h1>
-                  <p>nothing really</p>
+                  <h1>${cfg.domain}</h1>
+                  <h3>Nginx virtualhosts</h3>
+                  <ul>
+                    ${toListItems proxiedLinks}
+                  </ul>
+                  <h3>Unproxied Links</h3>
+                  <p><i>Not all of these are available outside the host</i></p>
+                  <ul>
+                    ${toListItems unproxiedLinks}
+                  </ul>
                 </body>
                 </html>
               '';
