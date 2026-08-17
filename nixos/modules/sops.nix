d0c741c43c5b00c5af233660d9ec429cc3f7b6d4
Elanora Manson <git@elanora.lol>
2026-08-17 13:48:44 -0700

    Add wireguard file secret

--- nixos/modules/sops.nix
+++ nixos/modules/sops.nix
@@ -2,10 +2,19 @@
 {
   imports = [ inputs.sops-nix.nixosModules.sops ];
 
-  sops = {
-    defaultSopsFile = ../../secrets/secrets.yaml;
-    defaultSopsFormat = "yaml";
+  sops =
+    let
+      secretsFolderPath = ../../secrets;
+    in
+    {
+      defaultSopsFile = secretsFolderPath + /secrets.yaml;
+      defaultSopsFormat = "yaml";
 
-    age.keyFile = "/home/el/.config/sops/age/keys.txt";
-  };
+      age.keyFile = "/home/el/.config/sops/age/keys.txt";
+
+      secrets."wireguard_conf" = {
+        sopsFile = secretsFolderPath + /wireguard.ini;
+        format = "ini";
+      };
+    };
 }
